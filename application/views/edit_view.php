636b7d919078ccd291834faa2b2cce4093344fed
Adriaan Louw <adriaanlw@gmail.com>
2012-07-18 15:57:54 +0200

    Completed the Tiny MCE integration into the create and edit news pages

--- application/views/edit_view.php
+++ application/views/edit_view.php
@@ -3,7 +3,34 @@
 <title>Edit News Entry</title>
 </head>
 <body>
+<script type="text/javascript" src="../../js/tiny_mce/tiny_mce.js"></script>
+<script type="text/javascript">
 
+tinyMCE.init({
+
+	// General options
+
+	mode : "textareas",
+	theme : "advanced",
+	plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,wordcount,advlist,autosave",
+
+	// Theme options
+	
+	theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+	theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+	theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+	theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,spellchecker,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,blockquote,pagebreak",
+	theme_advanced_toolbar_location : "top",
+	theme_advanced_toolbar_align : "left",
+	theme_advanced_statusbar_location : "bottom",
+	theme_advanced_resizing : true,
+	
+	skin : "o2k7",
+	skin_variant : "silver",
+
+});
+
+</script>
 <?php echo validation_errors(); ?>
 
 <?php if (isset($error)) echo $error; ?>
@@ -18,7 +45,7 @@
 <h4>Content</h4>
 <textarea name="content" required value="" cols="40" rows="10"><?php echo $entry->content ?></textarea>
 <br/>
-<input type="submit" value="Update Post" />
+<input type="submit" value="Update Post" formnovalidate="true"/>
 <?php endforeach; ?>
 </form>
 
