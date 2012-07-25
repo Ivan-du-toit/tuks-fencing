1bc28648503e302f6d4777cf3b907493e9d1ba2d
Jacques <jacqueslctz@gmail.com>
2012-07-25 11:13:48 +0200

    Tweek

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -8,10 +8,13 @@
 							<a href="web/about_us"><img src="images/About_Us.jpg" alt="About Us"/></a>
 						</div>
 						<div id="latestNews" style="overflow: hidden;">
+							<h2>Latest News</h2>
+							<hr />
 							<?php foreach ($entries as $entry) :?>
 							<h3><?php echo $entry->title; ?></h3>
+							<p id="latest_news_time"><?php echo 'Posted on '.$entry->date; ?></p>
+							<hr />
 							<p><?php echo $entry->content; ?></p>
-							<p><b><?php echo 'Posted on '.$entry->date; ?></b></p>
 							<?php endforeach; ?>
 							
 							
